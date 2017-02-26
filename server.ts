36e95fe2cd96f1cc69e3dce9fcba397d9319b46a
Vignesh <nvignesh86@gmail.com>
2017-02-27 02:09:38 +0530

    Mean

---
+++ server.ts
@@ -0,0 +1,129 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as root from 'app-root-path';
+import * as cookieParser from 'cookie-parser';
+import * as routes from './server/routes';
+import * as http from 'http';
+import * as debug from 'debug';
+
+
+const app = express();
+
+console.log(`${root}`);
+app.use(express.static(path.join(`${root}`,'client')));
+app.use(express.static(path.join(`${root}`,'client/dist')));
+
+// binding to console
+let log = debug('modern-express:server');
+log.log = console.log.bind(console);
+
+/**
+ * Get port from environment and store in Express.
+ */
+let PORT = process.env.PORT || '3000';
+
+
+// view engine setup
+app.set('views', `${root}/server/views/`);
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(cookieParser());
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    let err: any = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err: any, req: express.Request, res: express.Response, next: Function) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+function getPort(val) {
+    /**
+     * Normalize a port into a number, string, or false.
+     */
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+}
+
+app.set('port', PORT);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(PORT);
+
+server.on('error', (error) => {
+    /**
+     * Event listener for HTTP server "error" event.
+     */
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const bind = typeof PORT === 'string' ? `Pipe ${PORT}` : `Port ${PORT}`;
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+});
+
+server.on('listening', () => {
+    /**
+     * Event listener for HTTP server "listening" event.
+     */
+    const addr = server.address();
+    const bind = (typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`);
+    console.log(`Listening on ${bind}`);
+});
